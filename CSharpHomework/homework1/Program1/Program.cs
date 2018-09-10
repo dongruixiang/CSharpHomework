e5c23fc914df6532a3eddd8ec0fbd9e922279eb2
dongruixiang <358215517@qq.com>
2018-09-10 23:01:20 +0800

    init

---
+++ CSharpHomework/homework1/Program1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            int n = 0;
+            int m = 0;
+            int c = 0;
+            Console.Write("Please input the first int:");
+            s = Console.ReadLine();
+            n = Int32.Parse(s);
+            Console.Write("Please input the second int:");
+            s = Console.ReadLine();
+            m = Int32.Parse(s);
+            c = m * n;
+            Console.WriteLine("You have entered:" + n +"and" + m );
+            Console.Write("The product is " + c);
+        }
+    }
+}
